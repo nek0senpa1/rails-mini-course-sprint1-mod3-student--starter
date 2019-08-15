67c87d5bfcf5f9dec871f2a97a794c15ed976591
amberm <nekosenpai@gmail.com>
2019-08-15 14:30:22 -0600

    working with new ruby v on win10

--- tasks/app/controllers/tasks_controller.rb
+++ tasks/app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update,]
+  before_action :set_task, only: [:show, :edit, :update]
 
   # GET /tasks
   # GET /tasks.json
